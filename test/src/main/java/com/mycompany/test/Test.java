6e5b4e69c73bc07aa6b076121707465b037b7d68
GiannisMyro <ioannis.mironis@gmail.com>
2024-09-24 00:55:56 +0300

    test

---
+++ test/src/main/java/com/mycompany/test/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */hgjjjghjghjghjghjgh
+
+package com.mycompany.test;
+
+/**
+ *
+ * @author Giannis
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
